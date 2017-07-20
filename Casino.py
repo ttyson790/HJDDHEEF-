b19910558b427f7ee33300c0e5d689a2eeda65c7
ttyson790 <30300393+ttyson790@users.noreply.github.com>
2017-07-20 15:46:40 -0400

    Add files via upload

--- Casino.py
+++ Casino.py
@@ -1,54 +1,139 @@
-
-balance = 1
+balance = 1 
 def restart() :
-    print ("Welcome to casino! You start with 1$ and your goal is to get 100$! Good luck!")
     global balance
-    balance = 1    
+    print ("Welcome to casino! You start with 1$ and your goal is to get 100$! Good luck!")   
     def start() :
-    
-        game = input ("Please input which game you would like to play: [1] Coinflip   ")
         global balance
+        game = input ("Please input which game you would like to play: [1] Coinflip [2] Roulette       [3] Dice")
         if game == "1":
             import random
             flip = (random.random())
             bet = input ("How much would you like to bet? Minimum: 0.1   ")
             bet = float(bet)
 
-            if bet <= balance:   
+            if bet<= balance:   
                 balance -= bet
                 print ("Your balance is: ")
                 print (balance)
             else:
-                print ("You don't have enough money")
-                enter = input ("Press enter to go back, type restart to restart.   ")
-                if enter == ("restart"):
-                        restart()
-                elif enter == (""):
+                enter = input ("You don't have enough money. Press enter to go back or restart to restart.   ")
+                if enter == (""):
                     start()
-            
-            enter = input ("Press enter to flip.")
+                elif enter == ("restart"):
+                    restart()
+                else:
+                    print ("You didn't press enter")
+                    enter = input ("Press enter to go back.   ")
+                    if enter == (""):
+                        start()
+                    else:
+                        print ("You didn't press enter or type restart, you have been redirected to the main menu.")
+                        start()
+                        
+            enter = input ("Press enter to flip.   ")
             if enter == (""):
                 if flip >= 0.5:
                     print ("You won!")
                     balance += bet * 1.95
                     print ("Your balance is: ")
                     print (balance)
+                    enter = input ("Press enter to go back or type restart to restart.   ")
+                    if enter == (""):
+                        start()
+                    elif enter == ("restart"):
+                        restart()
+                    else:
+                        print ("You didn't press enter or write restart.")
+                        enter = input ("Press enter to go back or type restart to restart.   ")
+                        if enter == (""):
+                            start()
+                        elif enter == ("restart"):
+                            restart()
+                        else:
+                            print ("You didn't press enter, you have been redirected to the main menu.")
+                            start()
                 else:
                     print ("You lost!")
                     print ("Your balance is: ")
                     print (balance)
-                    cnrs = input ("Press enter to continue, type restart to restart.   ")
+                    cnrs = input ("Press any key to continue, type restart to restart.   ")
                     if cnrs == ("restart"):
                         restart()
+                    else: start()
             else:
-                print ("You didnt press enter")
-
-            enter = input ("Press enter to go back")
-
-            if enter == (""):
-                start()
+                print ("You didn't press enter.")
+                enter = input ("Press enter to go back to the main menu.   ")
+                if enter == (""):
+                    start()
+                else:
+                    print ("You didn't press enter, you have been redirected to the main menu.")
+                    start()
+        elif game == "2":
+            print ("Not avaliable yet.")
+            start()
+        elif game == "3":
+            bet = input ("How much would you like to bet? Minimum: 0.1   ")
+            bet = float(bet)  
+            if bet<= balance:   
+                balance -= bet
+                print ("Your balance is: ")
+                print (balance)
+            else:
+                enter = input ("You don't have enough money. Press enter to go back or restart to restart.   ")
+                if enter == (""):
+                    start()
+                elif enter == ("restart"):
+                    restart()
+                else:
+                    print ("You didn't press enter")
+                    enter = input ("Press enter to go back.   ")
+                    if enter == (""):
+                        start()
+                    else:
+                        print ("You didn't press enter or type restart, you have been redirected to the main menu.")
+                        start()
+            multiplier = input("Please set your multiplier. x2, x3, x5, x10, x20")
+            if multiplier == ("2" or "x2"):
+                enter = input ("Press enter to roll.   ")
+                if enter == (""):
+                    if flip >= 0.5:
+                        print ("You won!")
+                        balance += bet * 1.95
+                        print ("Your balance is: ")
+                        print (balance)
+                        enter = input ("Press enter to go back or type restart to restart.   ")
+                        if enter == (""):
+                            start()
+                        elif enter == ("restart"):
+                            restart()
+                        else:
+                            print ("You didn't press enter or write restart.")
+                            enter = input ("Press enter to go back or type restart to restart.   ")
+                            if enter == (""):
+                                start()
+                            elif enter == ("restart"):
+                                restart()
+                            else:
+                                print ("You didn't press enter, you have been redirected to the main menu.")
+                                start()
+                    else:
+                        print ("You lost!")
+                        print ("Your balance is: ")
+                        print (balance)
+                        cnrs = input ("Press any key to continue, type restart to restart.   ")
+                        if cnrs == ("restart"):
+                            restart()
+                        else:
+                            start()
+                
+                
+                
+            '''print ("Not avaliable yet.")
+            start()'''
         else:
-            print ("You didn't press enter")
+            print ("You did not input a valid game number.")
+            start()
     start()
 restart()
-
+        
+    
---
+++ Python Basics!.py
@@ -0,0 +1,110 @@
+"""
+num = 2
+
+addition = 50 + 75
+subtraction = 100 - 30
+multiplication = 4 * 4
+division = 80/10
+
+print (addition)
+print (subtraction)
+print (multiplication)
+print (division)
+
+exponent = 5**7
+modulo = 15%4
+# remainder of division
+
+print ("input the cost of the meal")
+cost = raw_input
+tax = 0.13
+tip = 0.15
+tcost = (cost +(cost * tax))
+
+print (tcost + (tcost * tip))
+
+fruit = 'APPLE'
+print (fruit.lower())
+fruit = 'apple'
+print (fruit.upper())
+
+print ('red' + 'hat')
+
+print ('what is your name')
+name = input ('')
+print ('Your name is ' + name + '?')
+i = input ('')
+if i = 'Yes'
+
+num1 = 1
+num2 = 2
+
+if num1 == num2 :
+    print ("The world is broken. My life is a lie.")
+    
+else : print ("Thank god that 2 doesnt = 1.")
+
+
+value1 = False
+value2 = False
+value3 = False
+value4 = True
+value5 = False
+
+value1 = False
+value2 = False
+value3 = True
+
+print ("you see 5 trees. one oak tree 2 cedar trees and 2 pine trees. what tree do you chop down?")
+print ("a. oak")
+print ("b. cedar")
+print ("c. pine")
+
+tree = input ('')
+
+if tree == 'a' :
+    print ("You proceed to hack away at the oak tree. As you are swinging your axe the tree makes a loud earsplitting crack and falls directly on your head, crushing your skull. YOU DIE.")
+if tree == 'b':
+    print ("You proceed to hack away at the cedar tree. The cedar tree wakes up and strangles you with its long branches. YOU DIE.")
+else : print ("You attempt to cut down the pine tree. Its too spiky! You back down. As you are backing up you fall into a pit that you didn't notice before.")
+
+value1 = True
+value2 = False
+value3 = False
+
+value1 = True
+value2 = True
+value3 = False
+
+
+name = "Bob"
+if 5 > 7 or 5 == 4.9:
+    print ("DUN DUN DUUUUUUUUUHHHH")
+elif name == "Sup Fam!":
+    print ("My name is bob")
+elif 5 + 5 + 5 and 10 + 5 == 15:
+    print ("True")
+
+fuuuuu = input ("FUU? ")
+if fuuuuu == ("Ya"):
+    print ("Ok fammmm")
+    
+
+
+word = input ("Enter your word   ")
+length = (len(word))
+wordfin = (word[1:length] + word[0:1] + "ay")
+print (wordfin.lower())
+x = 1
+if x < 0:
+     x = 0
+     print('Negative changed to zero')
+elif x == 0:
+     print('Zero')
+elif x == 1:
+     print('Single')
+else:
+     print('More')
+
+     
+    """
